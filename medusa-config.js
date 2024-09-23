eeec3ae71f3bf41bae98d0a196b640fce32dcf5b
Ethan Muller <ethanbmuller@gmail.com>
2024-09-23 18:54:25 -0400

    update backend_url

--- medusa-config.js
+++ medusa-config.js
@@ -80,6 +80,7 @@ const projectConfig = {
   admin_cors: ADMIN_CORS,
   // Uncomment the following lines to enable REDIS
   redis_url: REDIS_URL,
+  backend_url: "https://woolnut.mush.network",
   database_extra: process.env.NODE_ENV !== "development" ?
   {
     ssl: {
